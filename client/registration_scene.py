65764070f8e59999cd6d931b7612691bbe88d8a9
sangpham2710 <tansang.27.10.2003@gmail.com>
2024-04-06 20:39:33 +0700

    finish registration-scene

--- client/registration_scene.py
+++ client/registration_scene.py
@@ -18,34 +18,42 @@ class RegistrationScene(Scene):
 
         self.manager = pygame_gui.UIManager(SCREEN_SIZE)
 
+        self.background = pygame.image.load("client/assets/wallpaper-2.jpg")
         # Create a font object
-        self.font = pygame.font.Font(None, 50)  # Use the default font and a size of 50
+
+        self.title_font = pygame.font.Font("client/assets/Poppins-Regular.ttf", 72)
+        self.title_font.set_bold(False)
+        self.body_font = pygame.font.Font("client/assets/Poppins-Regular.ttf", 20)
+        self.body_font.set_bold(False)
 
         # Title
-        self.title_text = self.font.render(
-            "Racing Game", True, (0, 255, 255)
+        self.title_text = self.title_font.render(
+            "Racing Game", True, (200, 200, 200)
         )  # Cyan color
 
         # Username label
-        self.username_label_text = self.font.render(
-            "Username", True, (0, 255, 255)
+        self.username_text = self.body_font.render(
+            "Register Username", True, (255, 255, 255), (0, 0, 0)
         )  # Cyan color
 
         # Username input
         self.text_entry = pygame_gui.elements.UITextEntryLine(
-            relative_rect=pygame.Rect((200, 300), (400, 50)),
+            relative_rect=pygame.Rect((170, 515), (300, 50)),
             manager=self.manager,
         )
 
-        # Submit button
+        # Get the size of the screen
+        screen_width, screen_height = pygame.display.get_surface().get_size()
+
+        # Error message
+        self.error_message = self.body_font.render("", True, (200, 0, 0))
+
+        # Create the button
         self.button_submit = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect((350, 400), (100, 50)),
+            relative_rect=pygame.Rect((470, 515), (150, 50)),
             text="Submit",
             manager=self.manager,
         )
-
-        # Error message
-        self.error_message = self.font.render("", True, (255, 0, 0))
 
     def process_input(
         self, ui_events: List[pygame.event.Event], messages: queue.Queue
@@ -61,22 +69,22 @@ class RegistrationScene(Scene):
                             f"[UI Thread] [Registration] Username input: {username}"
                         )
                         if not all(char.isalnum() or char == "_" for char in username):
-                            self.error_message = self.font.render(
+                            self.error_message = self.body_font.render(
                                 "Username can only contain alphanumeric characters and underscores.",
-                                True,
-                                (255, 0, 0),
+                                False,
+                                (200, 0, 0),
                             )
                             return None
                         elif len(username) > 10:
-                            self.error_message = self.font.render(
+                            self.error_message = self.body_font.render(
                                 "Username cannot be longer than 10 characters.",
-                                True,
-                                (255, 0, 0),
+                                False,
+                                (200, 0, 0),
                             )
                             return None
                         elif len(username) == 0:
-                            self.error_message = self.font.render(
-                                "Username cannot be empty.", True, (255, 0, 0)
+                            self.error_message = self.body_font.render(
+                                "Username cannot be empty.", False, (200, 0, 0)
                             )
                             return None
                         else:
@@ -89,14 +97,25 @@ class RegistrationScene(Scene):
                 return LobbyScene()
             elif command == "REGISTRATION_FAILURE":
                 current_nickname = None
-                self.error_message = self.font.render(args[0], True, (255, 0, 0))
+                self.error_message = self.body_font.render(args[0], True, (200, 0, 0))
 
     def update(self, time_delta: float) -> None:
         self.manager.update(time_delta)
 
     def draw(self, screen: pygame.Surface) -> None:
-        screen.fill((0, 0, 50))  # Dark blue background
-        screen.blit(self.title_text, (200, 50))  # Draw the title
-        screen.blit(self.username_label_text, (200, 200))  # Draw the username label
-        screen.blit(self.error_message, (200, 500))  # Draw the error message
+        screen.blit(self.background, (0, 0))
+        # Get the size of the screen
+        screen_width, screen_height = pygame.display.get_surface().get_size()
+
+        # Calculate the position of the title
+        title_width, title_height = self.title_text.get_size()
+        title_x = (screen_width - title_width) / 2
+        title_y = (screen_height - title_height) / 2
+
+        # Draw the title
+        screen.blit(self.title_text, (title_x, 330))
+
+        # Draw the username_text
+        screen.blit(self.username_text, (170, 480))  # Draw the username label
+        screen.blit(self.error_message, (175, 565))  # Draw the error message
         self.manager.draw_ui(screen)
